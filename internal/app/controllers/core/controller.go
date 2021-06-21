6b6b8243e53635a8c326cf37ff607c9ea51ad783
zs368 <zs8153@163.com>
2021-06-21 22:28:45 +0800

    统一响应格式

---
+++ internal/app/controllers/core/controller.go
@@ -0,0 +1,51 @@
+package core
+
+import (
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+	"github.com/zs368/gin-example/pkg/errcode"
+	"github.com/zs368/gin-example/pkg/tools"
+)
+
+type Response struct {
+	Ctx *gin.Context
+}
+
+type Pager struct {
+	Page     int `json:"page"`
+	PageSize int `json:"page_size"`
+	Total    int `json:"total"`
+}
+
+func NewResponse(ctx *gin.Context) *Response {
+	return &Response{Ctx: ctx}
+}
+
+func (r Response) ToResponse(data interface{}) {
+	if data == nil {
+		data = gin.H{}
+	}
+	r.Ctx.JSON(http.StatusOK, data)
+}
+
+func (r Response) ToResponseList(list interface{}, total int) {
+	r.Ctx.JSON(http.StatusOK, gin.H{
+		"list": list,
+		"paper": Pager{
+			Page:     tools.GetPage(r.Ctx),
+			PageSize: tools.GetPageSize(r.Ctx),
+			Total:    total,
+		},
+	})
+}
+
+func (r Response) ToErrorResponse(err *errcode.Error) {
+	response := gin.H{"errcode": err.Code(), "errmsg": err.Msg()}
+	details := err.Details()
+	if len(details) > 0 {
+		response["details"] = details
+	}
+
+	r.Ctx.JSON(err.StatusCode(), response)
+}
