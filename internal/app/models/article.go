8a3da51171512e90771912e5069bbdb8345800e0
zs368 <zs8153@163.com>
2021-08-05 11:11:46 +0800

    gorm 钩子

--- internal/app/models/article.go
+++ internal/app/models/article.go
@@ -8,7 +8,7 @@ type Article struct {
 	Desc          string `json:"desc" gorm:"default:haha"`
 	CoverImageUrl string `json:"cover_image_url"`
 	Content       string `json:"content"`
-	Status        uint8  `json:"status"`
+	Status        uint8  `json:"status" gorm:"default:1"`
 	CreatedBy     string `json:"created_by"`
 	UpdatedBy     string `json:"updated_by"`
 }
@@ -19,5 +19,12 @@ func (Article) TableName() string {
 
 func (a *Article) BeforeCreate(tx *gorm.DB) error {
 	a.CoverImageUrl = "https://google.com/"
+	return nil
+}
+
+func (a *Article) BeforeUpdate(tx *gorm.DB) error {
+	if tx.Statement.Changed("title") {
+		tx.Statement.SetColumn("status", 1)
+	}
 	return nil
 }
